1a73452b8bd30f0ef351ed787c045da27d123148
jjou33 <zanda33@naver.com>
2023-01-18 15:24:40 +0900

    FEAT: 주석 제거

--- src/templates/landing_template.tsx
+++ src/templates/landing_template.tsx
@@ -289,58 +289,58 @@ const InteractiveLandingPage = ({
 
 export default InteractiveLandingPage
 
-// export const getPostList = graphql`
-//   query getPostList {
-//     site {
-//       siteMetadata {
-//         title
-//         description
-//         siteUrl
-//       }
-//     }
-//     allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___domain] }) {
-//       edges {
-//         node {
-//           id
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             title
-//             summary
-//             date(formatString: "YYYY.MM.DD.")
-//             categories
-//             index
-//             domain
-//             sideTitle
-//             thumbnail {
-//               childImageSharp {
-//                 gatsbyImageData(width: 768, height: 400)
-//               }
-//             }
-//             categoryIcon {
-//               childImageSharp {
-//                 gatsbyImageData(width: 10, height: 10)
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//     allFile(
-//       filter: {
-//         extension: { regex: "/(jpg)|(png)|(svg)|(gltf)|(bin)/" }
-//         sourceInstanceName: { eq: "images" }
-//       }
-//     ) {
-//       edges {
-//         node {
-//           extension
-//           sourceInstanceName
-//           id
-//           publicURL
-//         }
-//       }
-//     }
-//   }
-// `
+export const getPostList = graphql`
+  query getPostList {
+    site {
+      siteMetadata {
+        title
+        description
+        siteUrl
+      }
+    }
+    allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___domain] }) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            index
+            domain
+            sideTitle
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+            categoryIcon {
+              childImageSharp {
+                gatsbyImageData(width: 10, height: 10)
+              }
+            }
+          }
+        }
+      }
+    }
+    allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(png)|(svg)|(gltf)|(bin)/" }
+        sourceInstanceName: { eq: "images" }
+      }
+    ) {
+      edges {
+        node {
+          extension
+          sourceInstanceName
+          id
+          publicURL
+        }
+      }
+    }
+  }
+`
